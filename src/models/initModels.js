d423ebe5fb3fe1f2d51a4bd233b611ca91edec8e
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2022-12-09 21:40:58 -0600

    add quotes controllers and create service

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,10 +1,16 @@
 const Users = require('./users.models')
 const RecoveryPasswords = require('./recoveryPasswords.models')
+const Quotes = require('./quotes.models')
 
 const initModels = () => {
     //? FK = RecoveryPasswords
     Users.hasMany(RecoveryPasswords)
     RecoveryPasswords.belongsTo(Users)
+
+    //? FK = quotes
+    Quotes.belongsTo(Users)
+    Users.hasMany(Quotes)
+
 }
 
 module.exports = initModels
---
+++ src/quotes/quotes.services.js
@@ -0,0 +1,18 @@
+const quoteControllers = require('./quotes.controllers')
+
+
+const postQuote = (req, res) => {
+    const {quote, year} = req.body
+    const userId = req.user.id
+    quoteControllers.createQuote({quote, userId, year})
+        .then((data) => {
+            res.status(201).json(data)
+        })
+        .catch((err) => {
+            res.status(400).json({message: err.message})
+        })
+}
+
+
+
+
